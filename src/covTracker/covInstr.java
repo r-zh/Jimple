e54ab133ee07b6d7c993e72aae96248b9d2238bb
Rui <tonyzhou@live.cn>
2019-05-30 12:51:44 -0400

    log branch
    1. find the log statement
    2. find the exception method
    3. output the results into current path file
    	Jimple code lines
    	log code lines
    
    	Exception method number
    	Exception method size
    	log lines in Exception method size

--- src/covTracker/covInstr.java
+++ src/covTracker/covInstr.java
@@ -9,6 +9,9 @@
 package covTracker;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -28,6 +31,9 @@ import fault.StmtMapper;
 import soot.*;
 import soot.jimple.*;
 import soot.tagkit.Tag;
+import soot.toolkits.graph.Block;
+import soot.toolkits.graph.BlockGraph;
+import soot.toolkits.graph.ExceptionalBlockGraph;
 import utils.*;
 import soot.tagkit.LineNumberTag;
 
@@ -107,86 +113,210 @@ public class covInstr implements Extension {
 	
 	public int getSLOC() {
 		Set<Integer> lns = new HashSet<Integer>();
+		Set<Integer> lnsLog = new HashSet<Integer>();
 		/* traverse all classes */
 		Iterator<SootClass> clsIt = ProgramFlowGraph.inst().getUserClasses().iterator();
-		if (opts.instrall()) clsIt = ProgramFlowGraph.inst().getAppClasses().iterator(); 
+		if (opts.instrall())
+			clsIt = ProgramFlowGraph.inst().getAppClasses().iterator();
+
+		// define the parameter of result
+		int[] exception = new int[3];
+		int num_ex = 0, size_ex = 0, num_log_ex = 0;
+
 		while (clsIt.hasNext()) {
 			SootClass sClass = (SootClass) clsIt.next();
-			if ( sClass.isPhantom() ) {
+			if (sClass.isPhantom()) {
 				// skip phantom classes
 				continue;
 			}
-			if ( !sClass.isApplicationClass() ) {
+			if (!sClass.isApplicationClass()) {
 				// skip library classes
 				continue;
 			}
 
-            //if (sClass.isInterface()) continue;
-            //if (sClass.isInnerClass()) continue;
-			
 			/* traverse all methods of the class */
 			Iterator<SootMethod> meIt = sClass.getMethods().iterator();
 			while (meIt.hasNext()) {
 				SootMethod sMethod = (SootMethod) meIt.next();
-				if ( !sMethod.isConcrete() ) {
+				if (!sMethod.isConcrete()) {
 					// skip abstract methods and phantom methods, and native methods as well
-					continue; 
+					continue;
 				}
-				if ( sMethod.toString().indexOf(": java.lang.Class class$") != -1 ) {
+				if (sMethod.toString().indexOf(": java.lang.Class class$") != -1) {
 					// don't handle reflections now either
 					continue;
 				}
-				
+
 				// cannot instrument method event for a method without active body
-				if ( !sMethod.hasActiveBody() ) {
+				if (!sMethod.hasActiveBody()) {
 					continue;
 				}
 				/*
-				if (sMethod.isJavaLibraryMethod() || !sMethod.isDeclared() || sMethod.isNative()) {
-					continue;
-				}
-				*/
-				//if (sMethod.isConstructor() || sMethod.isStaticInitializer()) continue;
-				
-				/* the ID of a method to be used for identifying and indexing a method in the event maps of EAS */
+				 * if (sMethod.isJavaLibraryMethod() || !sMethod.isDeclared() ||
+				 * sMethod.isNative()) { continue; }
+				 */
+				// if (sMethod.isConstructor() || sMethod.isStaticInitializer()) continue;
+
+				/*
+				 * the ID of a method to be used for identifying and indexing a method in the
+				 * event maps of EAS
+				 */
 				String meId = sMethod.getSignature();
-				
-				/* 1. instrument method entry events and program start event */
+
 				CFG cfg = ProgramFlowGraph.inst().getCFG(sMethod);
-				
+
 				if (cfg == null || !cfg.isReachableFromEntry()) {
 					System.out.println("\nSkipped method unreachable from entry: " + meId + "!");
 					continue;
 				}
-				
+
 				// -- DEBUG
 				if (opts.debugOut()) {
 					System.out.println("\nNow instrumenting method " + meId + "...");
 				}
-
+				
 				for (CFGNode n : cfg.getNodes()) {
 					Stmt s = n.getStmt();
-					if (n.isInCatchBlock() || n.isSpecial() || s==null) continue;
-					if (s instanceof IdentityStmt) continue;
+					if (n.isInCatchBlock() || n.isSpecial() || s == null)
+						continue;
+					if (s instanceof IdentityStmt)
+						continue;
 					Tag lntag = null;
 					for (Tag t : s.getTags()) {
-						//System.out.println("one more tag " + t);
+						// System.out.println("one more tag " + t);
 						if (t instanceof LineNumberTag) {
 							lntag = t;
 							break;
 						}
 					}
-					if (lntag ==null) {
+					if (lntag == null) {
 						continue;
 					}
-						
+
 					byte[] arrln = lntag.getValue();
 					int ln = ((arrln[0] & 0xff) << 8) | (arrln[1] & 0xff);
+					
 					lns.add(ln);
+
+					// add log node into the list
+					if (isLLOC(s)) {
+						lnsLog.add(ln);
+						// System.out.println(s.toString());
+					}
 				}
-			} // -- while (meIt.hasNext()) 
+				
+				// add the num to the sum
+				exception = getException(sMethod);
+
+				num_ex += exception[0];
+				size_ex += exception[1];
+				num_log_ex += exception[2];
+
+			} // -- while (meIt.hasNext())
 		} // -- while (clsIt.hasNext())
+		System.out.println("Total Log Lines of Code in the program: " + lnsLog.size());
+
+		// total line, log line, exception number, size_ex, log number in
+		// exception size will be a problem, we don't really need it
+		String result = lns.size() + "," + lnsLog.size() + "," + num_ex + "," + size_ex + "," + num_log_ex;
+
+		// TODO echo -e "$1,\c" >> log_exception.csv
+		print_toFile(result);
 		return lns.size();
+	}
+
+	public boolean isLLOC(Stmt s) {
+		// function invoke
+		if (s instanceof InvokeStmt) {
+			InvokeExpr expr = s.getInvokeExpr();
+			/**
+			 * log logger timber system.out system.err info warn error debug fatal trace
+			 * aspect
+			 */
+			String log = expr.toString().toLowerCase();
+			String[] logSymbol = { ".log:", " println(java.lang.String)", ".logger:", ".timber:" };
+			// log statement symbols
+			for (String i : logSymbol) {
+				if (log.contains(i))
+					return true;
+			}
+		}
+		return false;
+	}
+
+	// when I add this condition, there is no code in the exception, so there are
+	// differences between the block graph and CFG, so we just count the number
+	public int[] getException(SootMethod sMethod) {
+		int[] exception = { 0, 0, 0 };
+
+		BlockGraph bg = new ExceptionalBlockGraph(sMethod.getActiveBody());
+
+		for (Block block : bg.getBlocks()) {
+			Stmt head = (Stmt) block.getHead(), curunit = head, exit = curunit;
+		
+			if (head.toString().contains(":= @caughtexception")) {
+				exception[0]++;
+				System.out.println(block.getBody());
+				
+				while (curunit != null) {
+					exit = curunit;
+					curunit = (Stmt) block.getSuccOf(curunit);
+				//System.out.println("here is the exception part:>>>>>>>>>>>>>>>>"+exit);
+
+					if(ProgramFlowGraph.inst().getNode((exit)).isInCatchBlock()){
+						System.out.println("it is isInCatchBlock:>>>>>>>>>>>>>>>>"+exit);
+						//continue;
+					}
+					if(ProgramFlowGraph.inst().getNode((exit)).isSpecial()) {
+						System.out.println("it is isSpecial:>>>>>>>>>>>>>>>>"+exit);
+						//continue;
+					}
+						
+					if (exit instanceof IdentityStmt)
+						continue;
+					Tag lntag = null;
+					for (Tag t : exit.getTags()) {
+						if (t instanceof LineNumberTag) {
+							lntag = t;
+							break;
+						}
+					}
+					if (lntag == null) {
+						continue;
+					}
+					System.out.println("it is counted:>>>>>>>>>>>>>>>>"+exit);
+					exception[1]++;
+
+					if (isLLOC(exit)) {
+						exception[2]++;
+					}
+				}//while (curunit != null
+			}
+		}//for (Block block : bg.getBlocks()) 
+		return exception;
+	}
+
+	public void print_toFile(String result) {
+		String apkName = apkDir.substring(apkDir.lastIndexOf('/') + 1, apkDir.lastIndexOf('.'));
+		String apkPath = apkDir.substring(0, apkDir.lastIndexOf('/') + 1);
+
+		result = apkName + "," + result;
+
+		// String filename = "../log_exception.csv";
+		String filename = apkPath + "log_exception.csv";
+		try {
+			FileOutputStream fos = new FileOutputStream(filename, true);
+			fos.write(result.getBytes());
+			fos.write("\n".getBytes());
+			fos.close();
+			System.out.println("File write successfully");
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+			System.out.println(e1);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println(e);
+		}
 	}
 		
 	public void instrument() {
